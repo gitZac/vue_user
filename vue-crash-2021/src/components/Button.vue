ef32abbe4079c461c80c27330cdd98dab24e0fcd
Zachary Taylor <zachary.taylor@L121-202106-01.local>
2021-12-09 14:46:55 -0500

    delete task

---
+++ vue-crash-2021/src/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+    <button @click="onClick()" :style="{background: bg_color}" class="btn">{{text}}</button>
+</template>
+
+<script>
+
+export default {
+    name: 'Button',
+    props: {
+        text: String,
+        bg_color: String
+    },
+    methods: {
+        onClick(){
+            console.log("Clicked");
+        }
+    }
+}
+</script>
---
+++ vue-crash-2021/src/components/header.vue
@@ -0,0 +1,35 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <Button bg_color="blue" text="Click Here" />
+    </header>
+</template>
+
+<script>
+import Button from './Button';
+
+export default {
+    name: 'Header',
+    props: {
+        title: {
+            type: String,
+            default: 'Title goes here'
+        }
+    },
+    components: {
+        Button, 
+    },
+}
+
+</script>
+
+<style scoped>
+
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+
+</style>
